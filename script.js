94c65da322911b245a19c56a5128dbab8c4f50f0
siyamtanzeel <67317028+siyamtanzeel@users.noreply.github.com>
2022-01-29 21:17:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -27,17 +27,15 @@ modeToggle.addEventListener('click',()=>{
     document.querySelector('i').classList.toggle('fas');
     document.querySelector('i').classList.toggle('far');
 })
-//প্রথমে তিনটি ইনপুট বক্সকেই সিলেক্ট করে variable এ store করব 
+
 const celciusInput = document.querySelector("#celVal");
 const farenheitInput = document.querySelector("#farVal");
 const kelvinInput = document.querySelector("#kelVal");
 
-// এরপরে একটি var এ সবগুলি ইনপুট বক্সকেই store করব 
 const inputs = document.querySelectorAll('input');
 
-// এরপর for loop চালিয়ে প্রত্যেকটি ইনপুট বক্সের জন্য আলাদা আলাদাভাবে 
+
 for(let i = 0;i< inputs.length;i++){
-    // একেকটি ইনপুটকে input নামক variable এ store করব 
     let input = inputs[i];
     input.addEventListener('input',(e)=>{
         let value = parseFloat(e.target.value);
@@ -58,69 +56,3 @@ for(let i = 0;i< inputs.length;i++){
         }
     })
 }
-
-
-
-
-
-
-// const methods = {
-//     celToFar : () =>{
-//         tempVal.placeholder = "Enter Celcius Temperature";
-//         secondaryPlaceholder.textContent = "Enter Celcius Temperature";
-//         resultTyp.innerHTML = '<sup>o</sup>' + 'F';
-
-//     },
-//     farToCel : () =>{
-//         tempVal.placeholder = "Enter Farenheit Temperature";
-//     },
-//     celToKel : () =>{
-//         tempVal.placeholder = "Enter Celcius Temperature";
-//     },
-//     kelToCel : () =>{
-//         tempVal.placeholder = "Enter Kelvin Temperature";
-//     },
-//     kelToFar : () =>{
-//         tempVal.placeholder = "Enter Kelvin Temperature";
-//     },
-//     farToKel : () =>{
-//         tempVal.placeholder = "Enter Farenheit Temperature";
-//     }
-// }
-
-
-
-
-
-
-// tempVal.addEventListener('input',(e) => {
-//     x = e.target.value;
-// })
-
-// //converterFunctions 
-// const calculations = {
-//     0: 0,
-//     1 : ()  =>{
-//         y = (x*9/5)+32;
-//     }
-// }
-// //functions for method btns 
-// convert.addEventListener('click',convertion)
-// const convertion = () => {
-//     calculations.i();
-//     resultVal.innerHTML = y;
-// }
-// const calculations = {
-//     method1: () =>{
-//         resultVal.innerHTML = x-32;
-//     }
-// }
-// darkToggle.addEventListener('click', () => {
-    //     document.querySelector('html').classList.toggle('dark')
-    //     bulb.classList.toggle('fas')
-    //     bulb.classList.toggle('far')
-    //     loginboxContainer.classList.toggle('bg-gradient-to-br')
-    //     loginboxContainer.classList.toggle('bg-background')
-    //     loginboxContainer.classList.toggle('bg-background-dark')
-    
-    // })
